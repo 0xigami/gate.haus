ddf69b61bcadf70873beb59d6e103716c62358b2
ART HAUS <mail@art.haus>
2021-08-31 12:50:21 +0200

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/analytics'
 import('firebase/functions')
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDQWki_Z-ZZibF-xJ9ser7-JGlbvIwHcqs', // don't care about using env vars for this because it will show up in the web site anyway
+  apiKey: 'AIzaSyDQWki_Z-ZZibF-xJ9ser7-JGlbvIwHcqs', 
   authDomain: 'gatehaus-6e1a0.firebaseapp.com',
+  databaseURL: 'https://gatehaus-6e1a0-default-rtdb.asia-southeast1.firebasedatabase.app', 
   projectId: 'gatehaus-6e1a0',
   storageBucket: 'gatehaus-6e1a0.appspot.com',
   messagingSenderId: '744875153943',
   appId: '1:744875153943:web:26cbfe81cef75c62af5a52',
-  //measurementId: 'G-EDT420QM09'
 }
 
 // Initialize Firebase
